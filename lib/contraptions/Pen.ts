f024ceece2f034f87e5994aa13bdd1f0ef53e326
Gideon Swaak <gideon.swaak@student.hu.nl>
2023-04-11 10:35:24 +0200

    Merge

--- lib/contraptions/Pen.ts
+++ lib/contraptions/Pen.ts
@@ -5,25 +5,31 @@ import { Vector2 } from '../math/Vectors';
 
 interface DrawBufferItem extends Vector2 {
 	color : string;
+	shadowColor : string;
 }
 
 export class Pen implements SceneObject {
 
 	public color : ((elapsedTime : number) => string);
+	public lineWidth : number = 1;
+	public shadowBlur : number = 0;
+	public shadowColor : ((elapsedTime : number) => string) = () => 'black';
 	private worldPosition: Vector2 = { x: 0, y: 0 };
 	private drawBuffer: DrawBufferItem[] = [];
 
-	constructor(private mountedAt: MountPoint, color : string | ((elapsedTime : number) => string)) {
-		if (typeof color === 'string') {
-			this.color = () => color;
-		} else {
-			this.color = color;
+	constructor(private mountedAt: MountPoint, color : string | ((elapsedTime : number) => string), options?: any) {
+		
+		this.color = typeof color === 'string' ? () => color : color;
+		if (options) {
+			if (options.lineWidth) this.lineWidth = options.lineWidth;
+			if (options.shadowBlur) this.shadowBlur = options.shadowBlur;
+			if (options.shadowColor) this.shadowColor = typeof options.shadowColor === 'string' ? () => options.shadowColor : options.shadowColor;
 		}
 	}
 
 	step(elapsedTime: number, deltaTime: number) {
 		transform(this.worldPosition, { x: 0, y: 0 }, this.mountedAt.transformation);
-		this.drawBuffer.push({ ...this.worldPosition, color: this.color(elapsedTime) });
+		this.drawBuffer.push({ ...this.worldPosition, color: this.color(elapsedTime), shadowColor: this.shadowColor(elapsedTime) });
 	};
 
 	drawDebug(context: CanvasRenderingContext2D) { };
@@ -38,6 +44,9 @@ export class Pen implements SceneObject {
 			}
 
 			context.strokeStyle = this.drawBuffer[0].color;
+			context.lineWidth = this.lineWidth;
+			context.shadowBlur = this.shadowBlur;
+			context.shadowColor = this.drawBuffer[0].shadowColor;
 			context.stroke();
 
 			this.drawBuffer = [ this.drawBuffer[this.drawBuffer.length - 1] ];
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	entry: './lib/index.ts',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.js',
+		filename: 'pintograph.js',
 		libraryTarget: 'module'
 	},
 	experiments: {
